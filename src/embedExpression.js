7543257e46df40e38e99f316436f526704be25fe
afahernandes <afahernandes@gmail.com>
2021-08-19 20:29:12 +0700

    2. Embedding expression

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -2,12 +2,23 @@ import React from "react";
 
 function EmbedExpression() {
   //Create function here
+  function getMajor() {
+    return "fullstack";
+  }
+  function getBatch(batch) {
+    return batch;
+  }
 
-  //Create a variable here
+
+
+  const companyName = "Dumbways.id";
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <h1>Welcome to{companyName}</h1>
+      <p>Major {getMajor()} Batch {getBatch(25)}</p>
+    </div>
   );
 }
 
